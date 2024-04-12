0fa4cae442a3bd598de4753f4efe848ee45a30c6
Muhammad Ali Shah <muhammad-ali.shah@capgemini.com>
2024-04-12 11:30:54 +0100

    Added feature flags

---
+++ src/Defra.Trade.ReMoS.AssuranceService.Shared/Constants/FeatureFlags.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Defra.Trade.ReMoS.AssuranceService.Shared.Constants;
+
+[ExcludeFromCodeCoverage]
+public static class FeatureFlags
+{
+    public const string SignUpApplication = "Nirms-SuS-SusToIdcomsSync";
+    public const string SelfServe = "Nirms-SuS-SelfServe";
+    public const string SelfServeMvpPlus = "Nirms-SuS-SelfServeMvpPlus";
+}
